afcc56e6298612e78264e3b032a9bee322ae35e3
Mr-mahato <cmhandan@gmail.com>
2024-12-31 22:54:56 +0530

    modified home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Search, List, Square, Table, ArrowDownUp, Filter } from "lucide-react";
+import kanbanRender from "../components/Kanban";
+import tableRnder from "../components/Table";
 function Home() {
+  const [trackHomeViewChoice, setTrackHomeViewChoice] =
+    useState<String>("kanban");
+
   return (
-    <div className="w-[90%]  mx-auto h-full">
+    <div className="w-[90%] mb-10 mx-auto h-full">
       <div className="flex justify-between p-4 border-b">
         <div className="flex gap-2 items-center">
-          <h1 className="self-center font-bold text-2xl ">Sites</h1>
+          <h1 className="self-center font-bold text-2xl">Sites</h1>
 
-          <div className="flex border cursor-pointer px-4 h-10 ">
+          <div
+            onClick={() => setTrackHomeViewChoice("List")}
+            className={`flex border cursor-pointer px-4 rounded-md h-10 ${
+              trackHomeViewChoice === "List" ? "bg-purple-600 text-white font-medium" : "bg-white"
+            }`}
+          >
             <List className="self-center h-4" />
-            <button className="self-center text-sm">List</button>
+            <button className={`self-center text-sm `}>List</button>
           </div>
 
-          <div className="flex  border cursor-pointer px-4 h-10">
+          <div
+            onClick={() => setTrackHomeViewChoice("kanban")}
+            className={`flex border cursor-pointer px-4 rounded-md h-10 ${
+              trackHomeViewChoice === "kanban" ? "bg-purple-600 text-white font-medium" : "bg-white"
+            }`}
+          >
             <Square className="self-center h-4" />
-            <button>Kanban</button>
+            <button name="kanban w-full">Kanban</button>
           </div>
-          <div className="flex border cursor-pointer px-4 h-10">
+          <div
+            onClick={() => setTrackHomeViewChoice("table")}
+            className={`flex border cursor-pointer px-4 rounded-md h-10 ${
+              trackHomeViewChoice === "table" ? "bg-purple-600 text-white font-medium" : "bg-white"
+            }`}
+          >
             <Table className="fitlerIconStyle" />
-            <button>Table</button>
+            <button name="table">Table</button>
           </div>
         </div>
 
         <div className="flex gap-2">
           <div className="filterButton">
-            <ArrowDownUp className="fitlerIconStyle"/>
+            <ArrowDownUp className="fitlerIconStyle" />
             <button>Sort By</button>
           </div>
-          <div className="filterButton">
-            <Filter className="fitlerIconStyle"/>
+          <div className="filterButton bg-purple-700 text-white font-medium rounded-md">
+            <Filter className="fitlerIconStyle" />
             <button>Filter</button>
           </div>
-          <div className="filterButton">
+          <div className="filterButton bg-purple-700 text-white font-medium rounded-md">
             {/* add durpin */}
-            <Search className="fitlerIconStyle"/>
+            <Search className="fitlerIconStyle" />
             <button>Find Menu</button>
           </div>
         </div>
       </div>
+
+      {/* lets start for the down one. */}
+      {/* lets render kanban */}
+
+      <div className="w-full mt-10 h-full flex justify-center items-center flex-wrap gap-4">
+        {kanbanRender}
+    </div>
     </div>
   );
 }
